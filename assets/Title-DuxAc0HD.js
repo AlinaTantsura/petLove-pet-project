e4fe8fd8449b7dfd0dca014e2e84145863eceaa2
Alina <alinatanst@gmail.com>
2024-08-01 13:43:50 +0300

    Updates

---
+++ assets/Title-DuxAc0HD.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-BQFosfhV.js";const x=({children:t})=>e.jsx("h1",{className:"font-bold text-[28px] md:text-[54px] leading-[100%] tracking-[-0.04em]",children:t});export{x as T};
