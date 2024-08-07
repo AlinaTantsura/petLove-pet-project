22d9ea191ebdc73cb546a1da48523ff68aa6c626
Alina <alinatanst@gmail.com>
2024-08-07 14:50:45 +0300

    Updates

--- assets/Title-BkXzO8Ng.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-6J86KVh8.js";const x=({children:t})=>e.jsx("h1",{className:"font-bold text-[28px] md:text-[54px] leading-[100%] tracking-[-0.04em]",children:t});export{x as T};
